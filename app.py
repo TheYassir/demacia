2c7f46ef772ba95d20c10653f1d346d73cedfbcc
johann <johann.chipan1@gmail.com>
2025-02-04 10:57:47 +0100

    📦 NEW: Base application

---
+++ app.py
@@ -0,0 +1,43 @@
+import os
+
+task  = ['Tomates','Pain']
+
+def ajout_tache():
+    return 1
+
+def complete_tache():
+    return 1
+
+def supprime_tache():
+    return 1
+
+def affiche_tache():
+    return 1
+
+def main():
+    while True:
+        print("\nTo-Do List")
+        print("1. Afficher les tâches")
+        print("2. Ajouter une tâche")
+        print("3. Afficher une tache")
+        print("4. Supprimer une tâche")
+        print("5. Quitter")
+        
+        choice = input("Choisissez une option : ")
+        
+        if choice == '1':
+            affiche_tache()
+        elif choice == '2':
+            ajout_tache()
+        elif choice == '3':
+            complete_tache()
+        elif choice == '4':
+            supprime_tache()
+        elif choice == '5':
+            print("Au revoir!")
+            break
+        else:
+            print("Option invalide. Veuillez réessayer.")
+
+if __name__ == "__main__":
+    main()
