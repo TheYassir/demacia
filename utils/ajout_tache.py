bf601408e73a0fe19ec86f30ee18bb7388b8b411
Honnok <Hujiklo.bn@gmail.com>
2025-02-04 13:55:53 +0100

    v3 ajout_tache.py

--- utils/ajout_tache.py
+++ utils/ajout_tache.py
@@ -1,8 +1,2 @@
-def ajout_tache(task_list):
-    nouvelle_tache = input("Entrez la nouvelle tâche : ").strip()
-
-    if nouvelle_tache:
-        task_list.append(nouvelle_tache)
-        print(f"✅ Tâche ajoutée : {nouvelle_tache}")
-    else:
-        print("⚠️ Impossible d'ajouter une tâche vide.")
+def ajout_tache():
+    return 1
